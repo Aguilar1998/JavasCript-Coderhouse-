dfcfea33d265144300aa046fbebb08cec2365d3d
MAguilar <aguilarmartin977@gmail.com>
2022-05-27 16:41:08 -0300

    Estructure mis carpetas y archivos para orientarlo a mi proyecto final ya definido. organice las carpetas, documente el proseso y cree una rama nueva para documentar la entrega del desafio nro 3

--- script/puerba.js
+++ script/puerba.js
@@ -1,113 +1,56 @@
-function mate(ingrediente_1, ingrediente_2, ingrediente_3, elSuperPreferido) {
-    return “La mejor forma de preparar un mate es: \n 1-Llenar un termo con ” + “agua ” +” caliente. Para que la temperatura de tu mate esté en su punto justo, asegurate de que no esté ni tibia ni hirviendo… \n” + ” 2-Colocar la “ + ingrediente_1 + ” dentro del mate, “ + ” preferentemente el que el publico elije es la “ + elSuperPreferido + ”, luego agregarle algun yuyos como “ + ingrediente_2 +  ” y agitar… \n 3-Le sacamos el polvo y luego le colocamos el “ + ingrediente_3 + ” \n 4- Introducimos la bombilla \n 5- Cebamos y compartimos un momento.”
+
+
+// Practica de javascript (ciclos y funciones)
+
+
+function tareas(tarea){
+    switch(tarea){
+        case "backend":
+            return "La tarea de backend esta asignada a Jesus Martin Aguilar"
+        case "frontend":
+            return "La tarea de frontend esta asignada a Jesus Eduardo Aguilar"
+        case "marketing":
+            return "La tarea de marketing esta asignada a Malena Guadalupe Aguilar"
+        case "emprendedor":
+            return "La tarea de emprendedor esta asignada a Fabian Lorenzo Aguilar"
+        case "cloud":
+            return "La tarea de cloud esta asignada a Ivan Nicolas Aguilar"
+        case "chef":
+            return "La tarea de chef esta asignada a Nilda Maria Alarcon"
+        default:
+            return "No hay persona asignada para esta tarea."
+        }
 }
-    
-    const pasos \= () \=> {
-    
-    let numRonda \= parseInt(
-    
-    prompt(“Ingrese la cantidad de personas en la ronda”)
-    
-    );
-    
-    var ingrediente_1 \= prompt(
-    
-    “El ingrediente que va primero es: \n Paso 1 (yerba) “
-    
-    ).toLowerCase();
-    
-    var ingrediente_2 \= prompt(
-    
-    “El ingrediente que va segundo es: \n Paso 2 (yuyo)”
-    
-    ).toLowerCase();
-    
-    var ingrediente_3 \= prompt(
-    
-    “El ingrediente qeu va tercero es: \n Paso 3 (agua) “
-    
-    ).toLowerCase();
-    
-    materos(numRonda, ingrediente_1, ingrediente_2, ingrediente_3);
-    
-    };
-    
-    const materos \= (numRonda, ingrediente_1, ingrediente_2, ingrediente_3) \=> {
-    
-    var elSuperPreferido \= 0;
-    
-    for (let i \= 1; i <= numRonda; i++) {
-    
-    let preferencia \= prompt(
-    
-    `Ingrese el tipo de yerba que prefiere \n Nro: #${i}`
-    
-    ).toLowerCase();
-    
-    let elPreferido1 \= 0;
-    
-    let elPreferido2 \= 0;
-    
-    let elPreferido3 \= 0;
-    
-    let elPreferido4 \= 0;
-    
-    // Comparo las preferencias y la asigno a una sola variable par aluego mostrar la preferida
-    
-    switch (preferencia) {
-    
-    case “yerbamate”:
-    
-    elPreferido1 += 1;
-    
-    break;
-    
-    case “aguantadora”:
-    
-    elPreferido2 += 1;
-    
-    break;
-    
-    case “taragui”:
-    
-    elPreferido3 += 1;
-    
-    break;
-    
-    case “rosamonte”:
-    
-    elPreferido4 += 1;
-    
-    break;
-    
-    }
-    
-    if (elPreferido1 > elPreferido2) {
-    
-    elSuperPreferido \= “yerbamate”;
-    
-    } else if (elPreferido2 > elPreferido3) {
-    
-    elSuperPreferido \= “aguantadora”;
-    
-    } else if (elPreferido3 > elPreferido4) {
-    
-    elSuperPreferido \= “taragui”;
-    
-    } else {
-    
-    elSuperPreferido \= “rosamonte”;
-    
-    }
-    
-    }
-    
-    console.log(
-    
-    mate(ingrediente_1, ingrediente_2, ingrediente_3, elSuperPreferido)
-    
-    );
-    
-    };
-    
-    pasos();
+
+let cantTareas = Number(parseInt(prompt("Ingrese la cantidad de tareas que hay pendientes para asignar.")))
+
+for(let i= 1; i<= cantTareas ; i++){
+    var tarea1 = tareas(prompt("Ingrese la tarea"))
+    console.log(tarea1)
+}
+tareas()
+
+
+
+// -----------------------------------------------------------------------------------
+
+
+// function miFuncionSolicitarNombre(primerNombre,segundoNombre,apellido,tarea) {
+//     return pedro = "La persona asignada a la tarea de " + tarea + " es: " + primerNombre + segundoNombre + apellido
+// };
+
+// var resultado1 = miFuncionSolicitarNombre("Jesus"," Martin"," Aguilar","backend");
+// var resultado2 = miFuncionSolicitarNombre("Lorenzo"," Fabian"," Aguilar","emprendedor");
+// var resultado3 = miFuncionSolicitarNombre("Malena"," Guadalupe"," Aguilar","marketing");
+// var resultado4 = miFuncionSolicitarNombre("Jesus"," Eduardo", " Aguilar","frontend");
+// var resultado5 = miFuncionSolicitarNombre("Ivan"," Nicolas", " Aguilar ","cloud");
+// var resultado6 = miFuncionSolicitarNombre("Nilda"," Maria", " Alarcon","chef");
+
+// console.log(resultado1)
+// console.log(resultado2)
+// console.log(resultado3)
+// console.log(resultado4)
+// console.log(resultado5)
+// console.log(resultado6)
+
+// miFuncionSolicitarNombre()
