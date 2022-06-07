7399fc3a303f6994451fea0abcaaecf34f7c1fc3
MAguilar <aguilarmartin977@gmail.com>
2022-06-07 18:53:56 -0300

    Cree un formulario y le implemente interaccion con DOM

--- script/javascript.js
+++ script/javascript.js
@@ -1,15 +1,14 @@
 
+
+
+
+
+
+
 class termos {
     constructor(marca, litros, precio) {
         this.marca = marca
         this.litros = litros
-        this.precio = precio
-    }
-}
-class mate {
-    constructor(marca, material, precio) {
-        this.marca = marca
-        this.material = material
         this.precio = precio
     }
 }
@@ -18,9 +17,13 @@ const termos1 = new termos('stanley ', 1, 24000,)
 const termos2 = new termos('enerfit', 1/2, 9000)
 const termos3 = new termos('thermos ', 1/2, 16000)
 
-const mate1 = new mate('stanley ', 'Aluminio', 24000,)
-const mate2 = new mate('enerfit', 'Aluminio', 9000)
-const mate3 = new mate('thermos', 'aluminio', 16000)
+// Creamos un array con todos nuestros productos dentro de termos
+const todosLosTermos = [
+    termos1,
+    termos2,
+    termos3
+]
+
 
 const carrito = []
 
@@ -28,10 +31,6 @@ const carrito = []
 const mostrarMensaje = (termos) => {
     console.log('El termos ' + termos.marca + ' de ' + termos.litros + ' cuesta $' + termos.precio)
 }
-const mostrarMensaje2 = (mate) => {
-    console.log('El termos ' + mate.marca + ' de ' + mate.litros + ' cuesta $' + mate.precio)
-}
-
 const totalCarrito = () => {
     let sumaTotal = 0
     carrito.forEach((termos) => {
@@ -41,12 +40,12 @@ const totalCarrito = () => {
 }
 
 const agregarProducto = () => {
-    const productoElegido = prompt('Elegi un termos entre: stanley, enerfit, thermos').toLowerCase()
+    const productoElegido = prompt('Elegi un termo entre: stanley, enerfit, thermos').toLowerCase()
 
     switch (productoElegido) {
         case 'stanley':
             mostrarMensaje(termos1)
-            carrito.push(termos1)
+            carrito.push(termos1)    
             break
         case 'enerfit':
             mostrarMensaje(termos2)
@@ -85,93 +84,70 @@ agregarProducto()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Desafio nro: 3
-// // function agunatadora(yerba){
-// //     yerba===variedadDeYerba[0]
-// // }
-
-
-// // function porCadaUno(array,funcion){
-// //     for(const elemento of array){
-// //         funcion(elemento)
-// //     }
-// // }
-
-// const variedadDeYerba = ["aguantadora","sinceridad","pampa","playadito","cbse","rosamonte"]
-
-// const mensaje = [
-//     { nombre: 'aguantadora',mensaje: "Desde Misiones, la Tierra del Oro Verde, una Yerba Mate única para acompañar tus jornadas. Encontrá la variedad que más se adapte a tus gustos en la Tienda Online"},
-//     { nombre: 'sinceridad', mensaje: ": Una Yerba Mate suave, pensada para quienes gustan de un mate muy equilibrado. Encontrá la variedad que más se adapte a tus gustos en la Tienda Online."},
-//     { nombre: 'pampa', mensaje: ": Un producto Premium. Cultivada y elaborada respetando los ritmos naturales de crecimiento. En versiones Orgánica y BCP (Bajo Contenido en Polvo)"},
-//     { nombre: 'playadito', mensaje: ": Sabor tradicional combinado con esencia de naranja. Técnica de estacionamiento natural y paquete pensado para conservar la calidad."},
-//     { nombre: 'cbse', mensaje: ": CBSé - Establecimiento Santa Ana es una empresa de bebidas orgánicas especializada en termos de yerba mate, con sede en Argentina. La empresa exporta sus termos a nivel internacional desde 2000. CBSé ofrece principalmente yerba mate de hojas sueltas con varios sabores."},
-//     { nombre: 'rosamonte', mensaje: ": Es nutritivo. Aporta vitaminas A, B, C y E, además de minerales como el potasio, sodio, hierro y magnesio a lo que se suman más de 10 aminoácidos esenciales. Previene la aparición de caries"}
-// ]
-
-// mensaje.forEach((nombre) => {
-//     console.log(nombre)
-// });
-
-
-
-
-// let cantYerbas = Number(parseInt(prompt("Ingrese la cantidad de marcas de yerbas que le gustaria conocer.")))
-
-// if( cantYerbas === variedadDeYerba.length){
-//     for(let i= 1; i<= variedadDeYerba.length; i++){
-//         const resultado = mensaje.filter((mensaje) => mensaje.nombre.includes('prog'))
-//         console.log(resultado)
-//     }
-// }else if(cantYerbas < variedadDeYerba.length){
-//     console.log("Contamos con estas variedades de yerbas: "+ variedadDeYerba )
-//     for(let i= 1; i<= cantYerbas; i++){
-//         const resultado = mensaje.filter((mensaje) => mensaje.nombre.includes('prog'))
-//         console.log(resultado)
-//     }
-// }else {
-//     console.log("no contamos con tantas varidedades por el momento, la informacion que no podamos brindarte sera investigada para incorporarla..")
-//     for(let i= 1; i<= cantYerbas; i++){
-//         const resultado = mensaje.filter((mensaje) => mensaje.nombre.includes('prog'))
-//         console.log(resultado)
-//     }
-// }
+/* FUNCIONES */
+const baseDeDatos = {
+    nombre: 'Martin',
+    correo: 'Martin@Estudiante.com',
+}
+const button = (event)=>{
+    event.preventDefault();
+    const nombre = event.target[0].value
+    const correo = event.target[1].value
     
-// // yerbas2()
+    if (!nombre || !correo) {
+        console.log('Falta completar un campo');
+        return;
+    }else if ( (nombre!==baseDeDatos.nombre)) {
+        console.log('Este proyecto no fue creado por ti!');
+        return;
+    }
+
+    console.log('Bienvenido',nombre,', tu correo es: ', correo);
+    // console.log(nombre);
+    // console.log(correo);
+};
 
 
-// // ---Tema:------
 
-// // retorna funciones 
-// // recibir funciones por parametros
-// // Métodos de búsqueda y transformación (forEach,find,filter,some,map,reduce,sort)
+const form = document.querySelector('#formulario');
+form.addEventListener('submit', button);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Agregar Nodos:
+// paso 1: crear un elemento en blanco
+// ej:
+    const contenedor = document.createElement('div')
+
+
+
+// paso 2: Darle un contenido a mi elemento creado
+// Se puede implementar una card completa? 
+    // ej:
+    contenedor.innerHTML = '<p>Para saber mas sobre nuestros productos registrate y envianos un mensaje para que podamos brindarte informacion al respecto.</p>'
+
+
+// paso 3: Asignar nuestro contenedor creado como hijo de otro elemento.
+// Antes de este paso, tengo que haber accedido a mi elemento padre.
+    // ej:
+    const galeria = document.getElementById('galeria')
+    galeria.append(contenedor)
+
+
+
+
+
+
