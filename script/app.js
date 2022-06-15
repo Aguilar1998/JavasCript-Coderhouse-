074581bd76b7475e7475e1cf611872ae1741c9fa
MAguilar <aguilarmartin977@gmail.com>
2022-06-15 11:18:53 -0300

    Cree una carpeta events.js para separar mi codigo y cree mas cards

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 
 
 // Declaraciones
-const allproductos = [termo, bombilla, mochilaMatera]
+const allproductos = [termo, bombilla, mochilaMatera, mate, yerba, taza]
 
 
 
@@ -36,8 +36,8 @@ const renderizarListaProductos = () =>{
             <button class="card__Buttom">
                 <span>Comprar</span>
             </button>
+            </div>
         </div>
-    </div>
         `;
         productosListContainer.append(productoCard)
     });
@@ -58,40 +58,6 @@ const renderizarListaProductos = () =>{
 
 
 // EventListeners
-
-
-
-// Eventos
-
-/* Funcion formulario */
-const baseDeDatos = {
-    nombre: 'Martin',
-    correo: 'Martin@Estudiante.com',
-}
-const button = (event)=>{
-    event.preventDefault();
-    const nombre = event.target[0].value
-    const correo = event.target[1].value
-    
-    if (!nombre || !correo) {
-        console.log('Falta completar un campo');
-        return;
-    }else if ( (nombre!==baseDeDatos.nombre)) {
-        console.log('Este proyecto no fue creado por ti!');
-        return;
-    }else if (!nombre && !correo){
-        window.location.href = 'http://www.google.com';
-        return;
-    }
-
-    console.log('Bienvenido',nombre,', tu correo es: ', correo);
-};
-
-
-
-const form = document.querySelector('#formulario');
-form.addEventListener('submit', button);
-
 
 
 
--- script/objeto.js
+++ script/objeto.js
@@ -11,5 +11,9 @@ class productos {
 const termo = new productos('Termo Stanley ', 'aluminio', 4000, 1,  'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/450/256/products/termo-verde-nuevo-frente1-9ebc319aca02038ab116166160261528-640-0.jpg' )
 const bombilla = new productos('Bombilla Campero', 'aluminio', 2000, 2, 'https://http2.mlstatic.com/D_NQ_NP_794871-MLA48950371330_012022-O.webp' )
 const mochilaMatera = new productos('Mochila Matera Enerfit ', 'aluminio', 1000, 3, 'https://http2.mlstatic.com/D_NQ_NP_999412-MLA49655019392_042022-O.webp' )
+const mate = new productos('Mate pampero ', 'madera', 1000, 3, '../assets/image/Mates/mate2-slider1.webp' )
+const yerba = new productos('Yerba aguantadora ', 'En paquete', 1000, 3, '../assets/image/yerbaAguantadora.jpg' )
+const taza = new productos('taza para cafe ', 'ceramica', 1000, 3, 'https://st.depositphotos.com/1020804/1263/i/450/depositphotos_12638853-stock-photo-coffee-cup-and-beans.jpg' )
 
 
+edadUsuario >= 18 ? console.log('puede pasar'): console.log('no puede pasar')
